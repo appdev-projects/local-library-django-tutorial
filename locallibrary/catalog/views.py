5df5a97b5770488e886d0adb8b84a5ec2b6bdc15
Ben Purinton <ben.purinton@gmail.com>
2024-04-27 17:50:38 +0000

    add two more queries to display on index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
     """View function for home page of site."""
@@ -14,12 +14,19 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_genres = Genre.objects.count()
+
+    # case insensitive regex filter
+    num_books_with_teacher = Book.objects.filter(title__iregex=r'teacher').count()
+
     # A dictionary we can pass to the template
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_teacher': num_books_with_teacher
     }
 
     # Render the HTML template index.html with the data in the context variable
